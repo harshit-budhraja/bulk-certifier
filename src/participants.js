66a737392be838207f03eb341cbb2f7e0dfe6052
Harshit Budhraja <harshitbudhraja1301@gmail.com>
2020-06-09 15:49:45 +0530

    Commented font embed.

--- src/participants.js
+++ src/participants.js
@@ -1,22 +1,22 @@
 const { degrees, PDFDocument, rgb } = require('pdf-lib');
-const { fontkit } = require('@pdf-lib/fontkit');
+// const { fontkit } = require('@pdf-lib/fontkit');
 const { readFile, writeFile } = require('./utilities');
 
 module.exports = async (participants) => {
-    const fontBytes = await readFile('./assets/Montserrat-SemiBold.ttf');
-    const Montserrat_SemiBold = fontkit.create(fontBytes);
+    // const fontBytes = await readFile('./assets/Montserrat-SemiBold.ttf');
+    // const Montserrat_SemiBold = fontkit.create(fontBytes);
     participants.forEach(async (participant) => {
         const pdfBytes = await readFile('./assets/template.pdf');
         const output_folder = 'output';
         const pdfDoc = await PDFDocument.load(pdfBytes);
-        await pdfDoc.embedFont(Montserrat_SemiBold);
+        // await pdfDoc.embedFont(Montserrat_SemiBold);
         const firstPage = pdfDoc.getPages()[0];
         const { width, height } = firstPage.getSize();
         firstPage.drawText(participant.name.toUpperCase(), {
             x: width / 2 - 100,
             y: height / 2 - 10,
             size: 21,
-            font: Montserrat_SemiBold,
+            // font: Montserrat_SemiBold,
             color: rgb(0, 0, 0),
             rotate: degrees(0),
         });
